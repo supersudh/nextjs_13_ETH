b833bebff7503205f795e9426a9bf47a667d8cfd
Sudharsan RaviKumar <supersudh@gmail.com>
2024-01-29 21:06:33 +0000

    Remove cache and logs

--- app/lib/Data.ts
+++ app/lib/Data.ts
@@ -2,13 +2,13 @@ import { FetchTransactions, fetchTransactionsServer } from "../utils/Query";
 
 export const fetchTransactions = async (options: FetchTransactions) => {
   try {
-    const lsData = window.localStorage.getItem('transfers');
-    if (lsData) {
-      console.log('Query.ts:cached...');
-      return JSON.parse(lsData);
-    }
+    // const lsData = window.localStorage.getItem('transfers');
+    // if (lsData) {
+    //   console.log('Query.ts:cached...');
+    //   return JSON.parse(lsData);
+    // }
     const data = await fetchTransactionsServer(options);
-    window.localStorage.setItem("transfers", JSON.stringify(data));
+    // window.localStorage.setItem("transfers", JSON.stringify(data));
     return data;
   } catch (error) {
     console.log('client:Error in fetching Transactions');
--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,7 +4,6 @@ describe('Application', () => {
   it(
     'should navigate to the root page, connect to a wallet and automatically display the transactions data',
     () => {
-      console.log('7 here', process.env.TEST_ADDRESS);
       cy.visit(HOSTNAME);
       it("should connect wallet with success", () => {
         cy.get("#connect-btn-MetaMask").click();
@@ -14,17 +13,5 @@ describe('Application', () => {
           process.env.TEST_ADDRESS
         );
       });
-
-      // it("import private key and connect wallet using imported metamask account", () => {
-      //   cy.importMetamaskAccount(
-      //     process.env.PRIVATE_KEY
-      //   );
-      //   cy.get("#connect-btn-MetaMask").click();
-      //   cy.acceptMetamaskAccess();
-      //   cy.get("#accounts").should(
-      //     "have.text",
-      //     "0x23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f"
-      //   );
-      // });
     });
 })
